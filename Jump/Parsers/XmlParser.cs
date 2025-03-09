c8e9b4947e36a3c45db6bb1d8dfdb6b85f65bc0c
Vincent Verboven <vincent.verboven85@gmail.com>
2025-03-09 21:16:09 +0100

    Implemented Xml content type

--- Jump/Parsers/XmlParser.cs
+++ Jump/Parsers/XmlParser.cs
@@ -1,11 +1,18 @@
-﻿namespace Jump.Parsers;
+﻿using System.Xml.Serialization;
+
+namespace Jump.Parsers;
 
 internal static class XmlParser
 {
     
     internal static async Task<object?> ParseBody(Stream body, Type paramType)
     {
-        throw new NotImplementedException("XmlParser is not implemented yet.");
+        using var reader = new StreamReader(body);
+        var xmlContent = await reader.ReadToEndAsync();
+        var xmlSerializer = new XmlSerializer(paramType);
+
+        using var stringReader = new StringReader(xmlContent);
+        return xmlSerializer.Deserialize(stringReader);
     }
     
 }
